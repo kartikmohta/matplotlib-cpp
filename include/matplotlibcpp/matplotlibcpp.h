30d56de3aec8e19edab5f81512731c285875976e
Kartik Mohta <kartikmohta@gmail.com>
2018-08-19 21:42:26 -0400

    Remove unused variable warning in errorbar

--- include/matplotlibcpp/matplotlibcpp.h
+++ include/matplotlibcpp/matplotlibcpp.h
@@ -484,7 +484,7 @@ bool loglog(const std::vector<NumericX>& x, const std::vector<NumericY>& y, cons
 }
 
 template<typename NumericX, typename NumericY>
-bool errorbar(const std::vector<NumericX> &x, const std::vector<NumericY> &y, const std::vector<NumericX> &yerr, const std::string &s = "")
+bool errorbar(const std::vector<NumericX> &x, const std::vector<NumericY> &y, const std::vector<NumericX> &yerr, const std::string &fmt = "")
 {
     assert(x.size() == y.size());
 
@@ -496,7 +496,8 @@ bool errorbar(const std::vector<NumericX> &x, const std::vector<NumericY> &y, co
 
     PyDict_SetItemString(kwargs, "yerr", yerrarray);
 
-    PyObject *pystring = PyString_FromString(s.c_str());
+    PyObject *pystring = PyString_FromString(fmt.c_str());
+    PyDict_SetItemString(kwargs, "fmt", pystring);
 
     PyObject *plot_args = PyTuple_New(2);
     PyTuple_SetItem(plot_args, 0, xarray);
